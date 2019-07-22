e8de07546b1d2e5a5222838388e9a57472702c6d
maurobisio <31115368+maurobisio@users.noreply.github.com>
2019-07-22 18:41:17 -0300

    reference

--- testjs/image-downloader.js
+++ testjs/image-downloader.js
@@ -1,6 +1,7 @@
 const download = require('image-downloader')
  
 // Download to a directory and save with the original filename
+//https://www.npmjs.com/package/image-downloader
 const options = {
   url: 'https://raw.githubusercontent.com/adam-p/markdown-here/master/src/common/images/icon48.png',
   dest: '../images'                  // Save to /path/to/dest/image.jpg
